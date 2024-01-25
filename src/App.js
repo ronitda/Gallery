e64283d9e8f60c22557ef7600ddcb960b4af5327
Roni Taboada <134081902+ronitda@users.noreply.github.com>
2024-01-25 18:57:55 +0200

    Milestone 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign: "center"}}>Image gallery</h1>
+    <Gallery/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,28 @@
+import React from  'react';
+
+
+const Gallery = () => {
+
+    let data = [
+        {
+            id: 1, 
+            imgSrc: ""
+        },
+        {
+            id: 2, 
+            imgSrc: ""
+        },
+        {
+            id: 3, 
+            imgSrc: ""
+        },
+    ]
+
+    return (
+
+        <h1>Gallery</h1>
+
+    )
+}
+
+export default Gallery;
